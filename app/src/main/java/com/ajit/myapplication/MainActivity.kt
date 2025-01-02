3ef412bd09fc0cffd58daea837c50fbd08b06bf5
VenusWhisper <bytewizardry@gmail.com>
2025-01-02 11:05:13 +0530

    Working app 2

--- app/src/main/java/com/ajit/myapplication/MainActivity.kt
+++ app/src/main/java/com/ajit/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ajit",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
